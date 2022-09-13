c18e8a5be08d0de75c8b7ba0e0032709690282cb
Ranjith Venkatesh <Ranjith.Venkatesh@mossandlichens.com>
2022-09-13 11:25:05 +0200

    Integrating Text stats

--- readability/index.js
+++ readability/index.js
@@ -1,3 +1,10 @@
 function onReadability(){
-    
+    var fleschKincaidGradeLevel = getFleschKincaidGradeLevel($("#kincaid").val(text_readability));
+    $("#kincaid").val(fleschKincaidGradeLevel);
+}
+
+function getFleschKincaidGradeLevel(inputText){
+    var TS = require('TextStatistics.js');
+    var ts = TS();
+    return ts.fleschKincaidGradeLevel(inputText);
 }
