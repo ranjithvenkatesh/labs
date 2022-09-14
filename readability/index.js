081baf985f886640f4ce5edaed2d5051135d96c1
Ranjith Venkatesh <Ranjith.Venkatesh@mossandlichens.com>
2022-09-14 11:51:46 +0200

    Another readability

--- readability/index.js
+++ readability/index.js
@@ -2,6 +2,7 @@ function onReadability(){
     var inputText = $("#text_readability").val();
     $("#kincaid").val(getFleschKincaidGradeLevel(inputText));
     $("#gunningFogScore").val(getGunningFogScore(inputText));
+    $("#colemanLiauIndex").val(getColemanLiauIndex(inputText));
 }
 
 function getFleschKincaidGradeLevel(inputText){
@@ -10,4 +11,8 @@ function getFleschKincaidGradeLevel(inputText){
 
 function getGunningFogScore(inputText){
     return TextStatistics.prototype.gunningFogScore(inputText);
+}
+
+function getColemanLiauIndex(inputText){
+    return TextStatistics.prototype.colemanLiauIndex(inputText);
 }
